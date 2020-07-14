bc619dbd5d4346879639eb7fe3b6f1779cd89de4
jlmooreiii <jloftonmooreiii@gmail.com>
2020-07-14 14:23:19 -0700

    board-and-boxes-done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react'
+import Square from './components/Square.js'
 import './App.css'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      squares: ["?1", "?2", "?3", "?4", "?5", "?6", "?7", "?8", "?9"]
     }
   }
 
   render(){
+    let square = this.state.squares.map((value, index) => {
+      return (
+        <Square 
+          value = {value}
+          index = {index}
+        />
+      )
+    })
     return(
       <React.Fragment>
         <h1>Treasure Hunt App</h1>
+        <div id = "board">
+          {square};
+        </div>
+
       </React.Fragment>
     )
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -4,6 +4,12 @@ class Square extends Component{
   render(){
     return(
       <React.Fragment>
+        <div id = "box"
+          style = {{width: "100px", height: "100px", border: "2px solid green"}}>
+          <div id = "box-text">
+            {this.props.value}
+          </div>  
+        </div>
       </React.Fragment>
     )
   }
