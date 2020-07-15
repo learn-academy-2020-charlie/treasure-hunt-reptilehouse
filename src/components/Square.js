28a86a0985e5c8d1b06dc0c509c4e96e0c9ac215
jlmooreiii <jloftonmooreiii@gmail.com>
2020-07-14 18:34:33 -0700

    need to finish end game function

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 
 class Square extends Component{
   
-  handleClick = () => {
-    this.props.alertIndex(this.props.index)
-  }
+  // handleClick = () => {
+  //   this.props.alertIndex(this.props.index)
+  // }
   
   handleClick2 = () => {
-    this.props.changeToTree(this.props.index)
+    this.props.checkBox(this.props.index)
   }
 
   render(){
@@ -15,7 +15,7 @@ class Square extends Component{
       <React.Fragment>
         <div id = "box"
           style = {{width: "100px", height: "100px", border: "2px solid green"}}
-          onClick = {this.handleClick, this.handleClick2}
+          onClick = {this.handleClick2}
           >
           <div id = "box-text">
             {this.props.value}
